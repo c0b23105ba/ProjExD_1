e06c6bc23e154701db3c02a36362d8f12c2eaf2f
Keita <c0b23105@edu.teu.ac.jp>
2024-09-24 16:50:28 +0900

    演習問題1:速度設定

--- pygame_image.py
+++ pygame_image.py
@@ -16,18 +16,20 @@ def main():
     kk_rct=kk_img.get_rect()
     kk_rct.center=300,200
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_list=pg.key.get_pressed()
         if key_list[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            kk_rct.move_ip((-1,-1))
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            kk_rct.move_ip((-1,+1))
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip((-1,-0))
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            kk_rct.move_ip((+2,0))
+        kk_rct.move_ip((-1,0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x,0 ])
         screen.blit(bg2_img,[x+1600,0])
